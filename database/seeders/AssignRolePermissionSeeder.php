d764d3ac516829158bca7e3ecb091452e89152a1
ddrag23 <kurodyami@gmail.com>
2023-08-27 16:13:26 +0700

    :sparkles: feat: add permission and sign role seeder

--- database/seeders/AssignRolePermissionSeeder.php
+++ database/seeders/AssignRolePermissionSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Constant\RoleConstant;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use Spatie\Permission\Models\Role;
 
 class AssignRolePermissionSeeder extends Seeder
@@ -14,9 +15,10 @@ class AssignRolePermissionSeeder extends Seeder
      */
     public function run(): void
     {
+        DB::table('role_has_permissions')->truncate();
         $superadmin = Role::where('name', RoleConstant::SUPERADMIN)->first();
         $admin = Role::where('name', RoleConstant::ADMIN)->first();
-        $superadmin->syncPermissions(['user-management', 'role-management', 'assign-permisson-role', 'assign-role-user', 'create-user', 'edit-user', 'delete-user', 'create-role', 'edit-role', 'delete-role']);
-        $admin->syncPermissions(['user-management', 'assign-role-user', 'create-user', 'edit-user', 'delete-user']);
+        $superadmin->syncPermissions(['dashboard', 'profile', 'user-management', 'role-management', 'assign-permisson-role', 'assign-role-user', 'create-user', 'edit-user', 'delete-user', 'create-role', 'edit-role', 'delete-role']);
+        $admin->syncPermissions(['dashboard', 'profile', 'user-management', 'assign-role-user', 'create-user', 'edit-user', 'delete-user']);
     }
 }
--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use Spatie\Permission\Models\Permission;
 
 class PermissionSeeder extends Seeder
@@ -13,7 +14,12 @@ class PermissionSeeder extends Seeder
      */
     public function run(): void
     {
-        $permissions = ['user-management', 'role-management', 'assign-permisson-role', 'assign-role-user', 'create-user', 'edit-user', 'delete-user', 'create-role', 'edit-role', 'delete-role'];
+        DB::table('model_has_permissions')->truncate();
+        DB::table('role_has_permissions')->truncate();
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+        Permission::truncate();
+        DB::statement('SET FOREIGN_KEY_CHECKS=1');
+        $permissions = ['dashboard', 'profile', 'user-management', 'role-management', 'assign-permisson-role', 'assign-role-user', 'create-user', 'edit-user', 'delete-user', 'create-role', 'edit-role', 'delete-role'];
         collect($permissions)->each(fn ($item) => Permission::create(['name' => $item]));
     }
 }
