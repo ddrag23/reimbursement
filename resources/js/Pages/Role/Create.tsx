db82f2496a500d5b341f9fb540c479e8362a6c15
ddrag23 <kurodyami@gmail.com>
2023-08-30 22:23:28 +0700

    :sparkles::bug: feat: add toast when same permission and fix autocomplete

--- resources/js/Pages/Role/Create.tsx
+++ resources/js/Pages/Role/Create.tsx
@@ -2,51 +2,76 @@ import AutoComplete from "@/Components/AutoComplete";
 import Card from "@/Components/Card";
 import Authenticated from "@/Layouts/AuthenticatedLayout";
 import { PageProps } from "@/types";
-import { Head } from "@inertiajs/react";
-import { useEffect, useState } from "react";
-
+import { Head, router } from "@inertiajs/react";
+import { FaTrash } from "react-icons/fa6";
+import { useEffect, useState, ReactNode } from "react";
+import toast, { Toaster } from 'react-hot-toast';
 type CreateProps = {
     title: string
     permissions: any[]
 }
+
+
 export default function Create({ auth, title, permissions }: PageProps<CreateProps>) {
-    const [value, setValue] = useState("");
+    const [value, setValue] = useState<string>("");
+    const [role, setRole] = useState<string>("");
     //a list of countries to show the dropdown
     const [permission, setPermissions] = useState<any[]>([])
 
     const [permissionData, setPermissionsData] = useState<string[]>([])
-
-    const handleInputChange = (value: string) => {
+    function handleInputChange(value: string) {
         if (value == '') {
             setPermissions(permissions)
             setValue(value);
             return
         }
         setValue(value);
-        setPermissionsData([...permissionData, value])
         const filtered = permission.filter(option =>
             option.toLowerCase().includes(value.toLowerCase())
         );
         setPermissions(filtered);
     };
+    function selectValue(value: string) {
+        if (permissionData.some(item => item == value)) {
+            toast.error("Data sudah ada")
+            return
+        }
+        setPermissionsData([...permissionData, value])
+        setValue("")
+        setPermissions(permissions)
+
+    }
+    function removePermissionData(item: string) {
+        setPermissionsData(permissionData.filter((i) => i != item))
+    }
+    function submit(e: any) {
+        e.preventDefault()
+        router.post(route('role.store'), { role_name: role, permissions: JSON.stringify(permissionData) })
+    }
+    function FooterCard() {
+        return <>
+            <div className="flex justify-end">
+                <button className="btn btn-primary" onClick={submit}>Simpan</button>
+            </div></>
+    }
     useEffect(() => {
         setPermissions(permissions)
     }, [])
     return <Authenticated user={auth.user}>
         <Head title={title} />
-        <Card title={title}>
+        <Card title={title} isFooter={true} childrenFooter={<FooterCard />}>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-3">
                 <div className="form-control w-full">
                     <label className="label">
                         <span className="label-text">Role Name</span>
                     </label>
-                    <input type="text" placeholder="Type here" className="input input-bordered w-full" />
+                    <input type="text" placeholder="Type here" className="input input-bordered w-full" value={role} onChange={(e) => setRole(e.target.value)} />
                 </div>
                 <div className="form-control w-full">
                     <label className="label">
                         <span className="label-text">Permissions</span>
                     </label>
-                    <AutoComplete value={value} onChange={handleInputChange} items={permission} />
+                    <AutoComplete value={value} onChange={handleInputChange} items={permission} handleClick={selectValue} />
                 </div>
             </div>
             <div className="grid grid-cols-1 mt-5">
@@ -60,13 +85,20 @@ export default function Create({ auth, title, permissions }: PageProps<CreatePro
                             </tr>
                         </thead>
                         <tbody>
-                            {permissionData.map(item => <tr>
+                            {permissionData.length ? permissionData.map((item, key) => <tr key={key}>
                                 <td>{item}</td>
-                            </tr>)}
+                                <td><button className="btn btn-error btn-sm" onClick={() => removePermissionData(item)}><FaTrash /></button></td>
+                            </tr>) : <tr><td colSpan={2} className="text-center text-gray-600">Tidak ada data</td></tr>}
                         </tbody>
                     </table>
                 </div>
             </div>
         </Card>
+        <Toaster position="top-center" toastOptions={{
+            style: {
+                background: '#363636',
+                color: '#fff',
+            },
+        }} />
     </Authenticated>
 }
