780b7ca0353fa541a78dc677ccaf1ec4f7c74b92
ddrag23 <kurodyami@gmail.com>
2023-09-02 16:50:21 +0700

    :sparkles: feat: add toolbar card

--- resources/js/Components/Card.tsx
+++ resources/js/Components/Card.tsx
@@ -1,17 +1,26 @@
+import { Link } from "@inertiajs/react";
 import type { ReactElement, ReactNode, PropsWithChildren } from "react";
-import React from "react";
+import { FaArrowLeft } from "react-icons/fa6";
 interface CardProps {
     title: string,
     children: ReactNode,
     isFooter?: boolean,
     childrenFooter?: ReactNode,
-    className?: string
+    className?: string,
+    toolBar?: ReactNode,
+    routeBack?: string
 }
-export default function Card({ title, children, isFooter, childrenFooter, className }: PropsWithChildren<CardProps>): ReactElement {
+export default function Card({ title, children, isFooter, childrenFooter, className, toolBar, routeBack }: PropsWithChildren<CardProps>): ReactElement {
     return <div className={`max-w-7xl mx-auto sm:px-6 lg:px-8 ${className}`}>
         <div className="bg-white dark:bg-gray-800 shadow-sm sm:rounded-lg">
-            <div className="pt-5 px-5">
-                <p className="font-bold">{title}</p>
+            <div className="flex justify-between items-center pt-5 px-5">
+                <div className="">
+                    <p className="font-bold">{title}</p>
+                </div>
+                <div className="flex gap-2">
+                    {routeBack && <Link href={routeBack as string} className="btn btn-sm btn-secondary"><FaArrowLeft />Kembali</Link>}
+                    {toolBar}
+                </div>
             </div>
             <div className="divider"></div>
             <div className="p-5">
